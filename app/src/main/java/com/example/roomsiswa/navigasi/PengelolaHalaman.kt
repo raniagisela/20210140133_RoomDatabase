87a0234ce9429f084dcd6574c567c56d457796c1
raniagisela <raniagslp@gmail.com>
2023-12-06 18:37:09 +0700

    add PengelolaHalaman.kt in navigasi

---
+++ app/src/main/java/com/example/roomsiswa/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,22 @@
+package com.example.roomsiswa.navigasi
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) = Unit
