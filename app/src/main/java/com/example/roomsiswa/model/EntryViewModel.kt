586a5c31c02a56249a523ebdba5c1d7ae8003573
raniagisela <raniagslp@gmail.com>
2023-12-06 18:11:00 +0700

    add data class in EntryViewModel

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.roomsiswa.model
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
@@ -25,3 +28,29 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
         }
     }
 }
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon,
+)
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon,
+)
